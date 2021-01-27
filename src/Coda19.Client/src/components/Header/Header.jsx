bb2469b0d62560b066501b8c066f81a5559d7737
borsanrazvan <razvanborsan@gmail.com>
2021-01-27 19:32:38 +0200

    Added History page

--- src/Coda19.Client/src/components/Header/Header.jsx
+++ src/Coda19.Client/src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 // import { Helmet } from 'react-helmet';
 import { Helmet } from 'react-helmet-async';
+import { useLocation } from 'react-router';
 
 import { Hidden } from '@material-ui/core';
 import AppBar from 'components/AppBar';
 import MainTitle from 'components/MainTitle';
+import ChinaMap from 'components/ChinaMap';
 import Statistics from 'components/Statistics';
 import PropTypes from 'prop-types';
 
@@ -14,24 +16,34 @@ import { pagesList } from './constants';
 
 import headerStyles from './Header.module.scss';
 
-const Header = ({ title, description }) => (
-  <div className={headerStyles.container}>
-    <Helmet>
-      <title>{title}</title>
-      <meta name="description" content={description} />
-    </Helmet>
-    <AppBar pagesList={pagesList} />
+const Header = ({ title, description }) => {
+  const location = useLocation();
+  return (
+    <div className={headerStyles.container}>
+      <Helmet>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Helmet>
+      <AppBar pagesList={pagesList} />
 
-    <Hidden smUp>
-      <TopRightSvg className={headerStyles.topRightSvg} />
-      <CenterSvg className={headerStyles.centerSvg} />
-    </Hidden>
+      <Hidden smUp>
+        <TopRightSvg className={headerStyles.topRightSvg} />
+        <CenterSvg className={headerStyles.centerSvg} />
+      </Hidden>
 
-    <MainTitle title={title} description={description} />
+      <div
+        className={
+          location.pathname === '/history' ? headerStyles.historyMainTitle : ''
+        }
+      >
+        <MainTitle title={title} description={description} />
+      </div>
 
-    <Statistics />
-  </div>
-);
+      {location.pathname === '/home' ? <Statistics /> : ''}
+      {location.pathname === '/history' ? <ChinaMap /> : ''}
+    </div>
+  );
+};
 export default Header;
 
 Header.propTypes = {
--- src/Coda19.Client/src/pages/History/History.jsx
+++ src/Coda19.Client/src/pages/History/History.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
 
-import EmptyCard from 'components/EmptyCard/EmptyCard';
+import { Grid, Typography } from '@material-ui/core';
 
 import { useHeaderDispatch } from 'contexts/header-context';
-import { PAGES } from 'shared/constants';
+import { lorem, PAGES } from 'shared/constants';
+
+import historyStyles from './History.module.scss';
 
 const History = () => {
   const headerDispatch = useHeaderDispatch();
@@ -13,9 +15,60 @@ const History = () => {
   }, []);
 
   return (
-    <section>
-      <EmptyCard />
-      <p>Hello!</p>
+    <section className={historyStyles.container}>
+      <Grid container justify="flex-start" direction="column">
+        <Grid item xs={12}>
+          <Typography variant="h4">HELLO THERE</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body1">{lorem}</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body1">{lorem}</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h4">HELLO THERE</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body1">{lorem}</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body1">{lorem}</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body1">{lorem}</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h4">HELLO THERE</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body1">{lorem}</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body1">{lorem}</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h4">HELLO THERE</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body1">{lorem}</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h4">HELLO THERE</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body1">{lorem}</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body1">{lorem}</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body1">{lorem}</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body1">{lorem}</Typography>
+        </Grid>
+      </Grid>
     </section>
   );
 };
