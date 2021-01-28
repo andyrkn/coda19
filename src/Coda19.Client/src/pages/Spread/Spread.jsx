be46cbba96af2ff4e6f8d0390dc5c7c4596ecf4b
borsanrazvan <razvanborsan@gmail.com>
2021-01-28 07:16:20 +0200

    Added Spread/Symptoms/Protection pages

--- src/Coda19.Client/src/pages/Spread/Spread.jsx
+++ src/Coda19.Client/src/pages/Spread/Spread.jsx
@@ -1,21 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import EmptyCard from 'components/EmptyCard/EmptyCard';
+import axios from 'axios';
 
 import { useHeaderDispatch } from 'contexts/header-context';
 import { PAGES } from 'shared/constants';
+import { Typography } from '@material-ui/core';
+
+import spreadStyles from './Spread.module.scss';
 
 const Spread = () => {
   const headerDispatch = useHeaderDispatch();
+  const [spread, setSpread] = useState();
 
   useEffect(() => {
     headerDispatch({ type: PAGES.SPREAD });
   }, []);
 
+  useEffect(() => {
+    axios
+      .get(`http://support.coda19.ashbell-platform.com/api/Spread`)
+      .then((res) => {
+        const { data } = res;
+        setSpread(data[0]);
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log(spread);
+  }, [spread]);
+
   return (
     <section>
-      <EmptyCard />
-      <p>hello!</p>
+      <div className={spreadStyles.container}>
+        <Typography variant="h3">{spread?.Title}</Typography>
+        <Typography variant="body2">{spread?.Description}</Typography>
+      </div>
     </section>
   );
 };
--- src/Coda19.Client/src/pages/Symptoms/Symptoms.jsx
+++ src/Coda19.Client/src/pages/Symptoms/Symptoms.jsx
@@ -1,21 +1,41 @@
-import React, { useEffect } from 'react';
-
-import EmptyCard from 'components/EmptyCard/EmptyCard';
+import React, { useEffect, useState } from 'react';
 
 import { useHeaderDispatch } from 'contexts/header-context';
 import { PAGES } from 'shared/constants';
 
+import axios from 'axios';
+
+import { Typography } from '@material-ui/core';
+
+import symptomsStyles from './Symptoms.module.scss';
+
 const Symptoms = () => {
   const headerDispatch = useHeaderDispatch();
+  const [symptoms, setSymptoms] = useState();
 
   useEffect(() => {
     headerDispatch({ type: PAGES.SYMPTOMS });
   }, []);
 
+  useEffect(() => {
+    console.log(symptoms);
+  }, [symptoms]);
+
+  useEffect(() => {
+    axios
+      .get(`http://support.coda19.ashbell-platform.com/api/Symptom`)
+      .then((res) => {
+        const { data } = res;
+        setSymptoms(data[0]);
+      });
+  }, []);
+
   return (
     <section>
-      <EmptyCard />
-      <p>hello!</p>
+      <div className={symptomsStyles.container}>
+        <Typography variant="h3">{symptoms?.Title}</Typography>
+        <Typography variant="body2">{symptoms?.Description}</Typography>
+      </div>
     </section>
   );
 };
