f0e2d16a6aa944fcc57bf3d65c3152fe448cadbb
Andrei Mihaila <Andrei.Mihaila@centric.eu>
2021-01-28 09:48:55 +0200

    move final sources

--- src/Coda19.Client/src/API/homeAPI.jsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import { useContext } from 'react';
-
-
-export const useGlobalCases = () => {
-  //const api = useContext(ApiContext);
-
-};
--- src/Coda19.Client/src/shared/helpers.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-
-export const upperFirstLetter = (string) =>
-  string[0].toUpperCase() + string.toLowerCase().slice(1);
